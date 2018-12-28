f1f2e2b780654e9b1f85851ece5de8f88969b689
jumo <ju.jumo@gmail.com>
2018-12-28 22:22:58 +0100

    update test samples (on demand).

--- test/test_parser_leboncoin.py
+++ test/test_parser_leboncoin.py
@@ -1,6 +1,31 @@
 import unittest
+import argparse, logging
 import ads.parsing.ParsingLeboncoin as lbc
 from ads.net import get_page_tree
+
+fake_samples = {
+    'page_single':
+        {'filename': 'sample_annonce_list_nopage.html',
+         'url': 'https://www.leboncoin.fr/recherche/?category=9&region=22&cities=38420&real_estate_type=1&square=130-max&rooms=4-max&price=min-600000'},
+    'page_lot':
+        {'filename': 'sample_annonce_list_pagination.html',
+         'url': 'https://www.leboncoin.fr/recherche/?category=9&region=22&cities=Grenoble_38000'},
+    'page_last':
+        {'filename': 'sample_annonce_list_last.html',
+         'url': 'https://www.leboncoin.fr/recherche/?category=9&regions=22&location=38190&page=7'},
+    'ads':
+        {'filename': 'sample_annonce_estate.html',
+         'url': 'https://www.leboncoin.fr/ventes_immobilieres/1365750706.htm/'},
+}
+
+
+def build_samples():
+    from ads.net import get_content_url, persistent_driver
+    for page in fake_samples.values():
+        logging.debug(f'creating sample file {page["filename"]}')
+        with open(page['filename'], 'w') as fout:
+            content = get_content_url(page['url'])
+            fout.write(content)
 
 
 class TestParserLeboncoinList(unittest.TestCase):
@@ -70,5 +95,18 @@ class TestParserLeboncoin(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    parser = argparse.ArgumentParser(description='launch unit test (or build sample files to test against).')
+    parser.add_argument('-v', '--verbose', action='count', default=0, help='verbosity level')
+    parser.add_argument('-s', '--samples', action='store_true', help='build samples to test against')
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.getLogger().setLevel(logging.INFO)
+    if args.verbose > 1:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    if args.samples:
+        build_samples()
+    else:
+        unittest.main()
 
